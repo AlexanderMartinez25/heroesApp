b3e0028cf3672a9aae916cc32a73f284439ab671
Alexander <alexander.martinez@lapatota.cl>
2022-06-24 18:43:53 -0400

    (routerNavigate) diseño pantalla heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
-import { switchMap } from "rxjs/operators";
+import { switchMap, tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    ` img {
+      width:100%;
+      border-radius: 5px
+    }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
@@ -16,7 +21,8 @@ export class HeroeComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private heroeService: HeroesService
+    private heroeService: HeroesService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -25,10 +31,14 @@ export class HeroeComponent implements OnInit {
     // al sericio con el parametro enviado
     this.route.params
       .pipe(
-        switchMap(({ id }) => this.heroeService.getHeroeById(id))
+        switchMap(({ id }) => this.heroeService.getHeroeById(id)),
+        tap(console.log)
       )
-      .subscribe(heroe => this.heroe = heroe)
+      .subscribe(heroe => this.heroe = heroe[0])
+  }
 
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 }
 
